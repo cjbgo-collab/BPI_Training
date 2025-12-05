0edcc96dce1a1d57c0accca48d99f225461b2eb5
Cathrina Jane B. Go <cjbgo@headoffice.corp.bpi.com.ph>
2025-12-05 21:53:14 +0800

    Initialize

---
+++ M2_Activity2.java
@@ -0,0 +1,13 @@
+package com.bpi.helloworld.main;
+
+public class M2_Activity2 {
+
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+		Whiteboard wb = new Whiteboard();
+		int Sum = wb.addNumbers(5, 3);
+		System.out.println("The total is: " + String.valueOf(Sum));
+
+	}
+
+}
---
+++ Whiteboard.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.bpi.helloworld.main;
+
+public class Whiteboard {
+
+	public int addNumbers(int addend1, int addend2) {
+		int sum1 = addend1 + addend2;
+		return sum1;
+	}
+
+}
