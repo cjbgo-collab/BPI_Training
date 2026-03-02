348a588a730b63d13ea635b5949adc639cbb4709
Cathrina Jane B. Go <cjbgo@headoffice.corp.bpi.com.ph>
2026-03-02 14:07:55 +0800

    M8_ACT9

---
+++ M8_Activity9/M8_Activity9/BookCreateRequest.java
@@ -0,0 +1,12 @@
+package M8_Activity9;
+
+public class BookCreateRequest {
+    private String title;
+    private String author;
+
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+}
